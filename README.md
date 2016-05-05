dfc40bf76d03f6f3d9b18ce28f069fd27a03faed
Spencer Caldwell <spencer.caldwell@gmail.com>
2016-05-05 12:32:52 -0700

    added some CSS and cleaned up JS code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#viz of education attainment of women 15-24
+#Visualization of average years of education for women age 15-24
+
+To run this visualization in a BASH terminal on your local machine, clone the repo, change directory into female_education_15_to_24_viz, run the command `npm install`, and run the command `node server.js`.  Then, in your web browser, navigate to `http://localhost:3000/map.html`.
--- app/map.js
+++ app/map.js
@@ -11,45 +11,36 @@ var projection = d3.geo.mercator()
 var path = d3.geo.path()
     .projection(projection);
 
-//***I need to resolve this part
 var color = d3.scale.linear()
   .range(['#f7fbff','#deebf7','#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5','#08519c','#08306b']);
 
 d3.csv('women_15_to_24.csv', function(data) {
-//***I need to resolve this part
   color.domain([0, 1]);
 
   d3.json('world.json', function(json) {
 
-    //this needs to be made more performant
+    //this could be made more performant
+    //puts education data in geojson object for specified year
     function colorData(year) {
-    for (var i = 0; i < data.length; i++) {
-      if(data[i].year === year) {
-        var locationCode = data[i].location_code;
-        var educationLevel = parseFloat(data[i].mean)
+      for (var i = 0; i < data.length; i++) {
+        if(data[i].year === year) {
+          var locationCode = data[i].location_code;
+          var educationLevel = parseFloat(data[i].mean);
 
-        for (var j = 0; j < json.features.length; j++) {
-          var jsonCountry = json.features[j].properties.iso_a3;
+          for (var j = 0; j < json.features.length; j++) {
+            var jsonCountry = json.features[j].properties.iso_a3;
 
-          if (locationCode === jsonCountry) {
-          json.features[j].properties.value = educationLevel;
-          json.features[j].properties.year = data[i].year
-          break;
+            if (locationCode === jsonCountry) {
+            json.features[j].properties.value = educationLevel;
+            break;
+            }
           }
         }
       }
-    }
     return json.features;
     }
 
-    // console.log(json);
-    // colorData('2010');
-    // console.log(json);
-    // colorData('1971');
-    // console.log(json);
-
-    // console.log(json.features);
-
+    //draws and colors the map on page load
     var mapColors = svg.selectAll('path')
       .data(colorData('1970'))
       .enter()
@@ -63,9 +54,7 @@ d3.csv('women_15_to_24.csv', function(data) {
           return color(value);
         }
     });
-      // .call(colorCountry);
 
-//new code goes here
     var label = svg.append('text')
       .attr('class', 'year label')
       .attr('text-anchor', 'end')
@@ -84,11 +73,7 @@ d3.csv('women_15_to_24.csv', function(data) {
       .attr('height', box.height)
       .on('mouseover', enableInteraction);
 
-    // function colorCountry(path) {
-    //   path.style('fill', function(d) {return d.properties.value;});
-    //   console.log('called colorCountry')
-    // }
-
+    //functionality for all mouse events
     function enableInteraction() {
       var yearScale = d3.scale.linear()
         .domain([1970, 2015])
@@ -114,41 +99,19 @@ d3.csv('women_15_to_24.csv', function(data) {
       }
     }
 
+    //updates label, recalculates data, and redraws map fill on mouse event
     function displayYear(year) {
       roundYear = (Math.round(year)).toString();
       label.text(roundYear);
-      console.log(roundYear);
-      console.log(year);
 
       svg.selectAll('path').data(colorData(roundYear))
-        // .call(colorCountry);
       .style('fill', function(d) {
         var value = d.properties.value;
-        // console.log(d.properties.year)
-        // console.log(value);
         if(value) {
           return color(value);
         }
       });
-
-      // svg.selectAll('path')
-      //   .data(colorData(year))
-      //   .enter()
-      //   .append('path')
-      //   .attr('d', path)
-      //   .attr('stroke', '#cccccc')
-      //   .attr('stroke-width', '1px')
-      //   .style('fill', function(d) {
-      //     var value = d.properties.value;
-      //     if(value) {
-      //       return color(value);
-      //     }
-      // });
     }
 
   });
 });
-
-
-//make a style function and call it
-//may need to use the key property
